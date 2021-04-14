89673146fb478d27c9a37c40386a2d3f596e5e20
Anders Lauvrak Espenes <andersespenes@hotmail.com>
2021-04-14 18:27:10 +0200

    Added json format support

--- app/src/main/java/com/example/todolist/data/TodoList.kt
+++ app/src/main/java/com/example/todolist/data/TodoList.kt
@@ -1,10 +1,14 @@
 package com.example.todolist.data
 
-import android.os.Parcel
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-data class TodoList(val text: String, val taskList: MutableList<Task>) : Parcelable
+data class TodoList(val text: String, val taskList: MutableList<Task>) : Parcelable {
+
+    override fun toString(): String {
+        return "TodoList [text: ${text}, taskList: ${taskList}]"
+    }
+}
 
 
